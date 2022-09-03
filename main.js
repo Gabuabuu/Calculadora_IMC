28c228fe1b187b01a745a67d296de3ccf3cd4e79
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-09-03 16:09:28 -0300

    finalizando índices do imc

--- main.js
+++ main.js
@@ -17,30 +17,29 @@ verificar.addEventListener('click', function(event) {
   
   let calcImc = imc.toFixed (1)
   
-  let resImc = ['18,5', '18,5 e 25', '25,0 e 29,9', '30,0 e 39,9']
+  let resImc = ['18,5', '18,5 e 25', '25,0 e 29,9', '30,0 e 39,9'] //array para identificar se o imc esta ou não na medida certa
   
-  imc = imc.toFixed(1)
+  imc = imc.toFixed(1) //fixando resultado do imc só até a primeira casa decimal
   
   console.log(resImc[1])
   if(imc < 18.5) {
     res.innerHTML = `Ola ${name}! Seu imc é de: ${imc} e está entre ${resImc[0]} ou abaixo || Classificaçao: Magreza || Grau de obesidade: 0`
     
   } else if(imc >= 18.5 && imc <= 24.9) {
-    res.innerHTML = ` Olá ${name}! Seu imc está entre: ${resImc[1]} || Classificaçao: Normal || Grau de obesidade: 0`
+    res.innerHTML = ` Olá ${name}! Seu imc é de ${imc} e está entre: ${resImc[1]} || Classificaçao: Normal || Grau de obesidade: 0`
     
   } else if (imc >= 25.0 && imc <= 29.9) {
-    res.innerHTML = `Olá ${name}! Seu imc está entre ${resImc[2]} || Classificaçao: Sobrepeso || Grau de obesidade: 1`
+    res.innerHTML = `Olá ${name}! Seu imc é de ${imc} e está entre ${resImc[2]} || Classificaçao: Sobrepeso || Grau de obesidade: 1`
     
   } else if (imc >= 30.0 && imc <= 39.9) {
     res.innerHTML = `Olá ${name}! Seu imc é  de ${imc} e está entre ${resImc[3]} || Classificaçao: Obesidade || Grau de obesidade: 2`
   } else  {
     imc > 40.0;
-    res.innerHTML = `Olá ${name}! Seu imc está acima de 40,0 || Classificaçao: Obesidade Grave || Grau de obesidade: 3`
-  }
+    res.innerHTML = `Olá ${name}! Seu imc é de ${imc} e está acima de 40,0 procure um médico urgente! || Classificaçao: Obesidade Grave || Grau de obesidade: 3`
+  } //checagem para dizer se o imc esta ou não na medida certa e dizendo em qual classificação se adequa.
     
 })
 
 const year = document.getElementById('current-year')
 
-year.innerHTML = new Date().getFullYear()
-
+year.innerHTML = new Date().getFullYear() //atualiza o ano do footer
