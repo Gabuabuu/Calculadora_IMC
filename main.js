bceaa1b9964571fbe21cdc5e7af91726e6f14a62
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-09-03 15:59:24 -0300

    adicionando os índices dos imc recomendados

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const calc = document.querySelector('#calc')
 
-calc.addEventListener('click', function(event) {
+verificar.addEventListener('click', function(event) {
   event.preventDefault()
   
   const name = document.getElementById('name').value 
@@ -17,9 +17,27 @@ calc.addEventListener('click', function(event) {
   
   let calcImc = imc.toFixed (1)
   
-  res.innerHTML = `Olá ${name}! Você tem ${height} de altura e pesa ${weight} kilos. Seu imc é de ${calcImc}`
+  let resImc = ['18,5', '18,5 e 25', '25,0 e 29,9', '30,0 e 39,9']
   
-  console.log(imc.toFixed(2))
+  imc = imc.toFixed(1)
+  
+  console.log(resImc[1])
+  if(imc < 18.5) {
+    res.innerHTML = `Ola ${name}! Seu imc é de: ${imc} e está entre ${resImc[0]} ou abaixo || Classificaçao: Magreza || Grau de obesidade: 0`
+    
+  } else if(imc >= 18.5 && imc <= 24.9) {
+    res.innerHTML = ` Olá ${name}! Seu imc está entre: ${resImc[1]} || Classificaçao: Normal || Grau de obesidade: 0`
+    
+  } else if (imc >= 25.0 && imc <= 29.9) {
+    res.innerHTML = `Olá ${name}! Seu imc está entre ${resImc[2]} || Classificaçao: Sobrepeso || Grau de obesidade: 1`
+    
+  } else if (imc >= 30.0 && imc <= 39.9) {
+    res.innerHTML = `Olá ${name}! Seu imc é  de ${imc} e está entre ${resImc[3]} || Classificaçao: Obesidade || Grau de obesidade: 2`
+  } else  {
+    imc > 40.0;
+    res.innerHTML = `Olá ${name}! Seu imc está acima de 40,0 || Classificaçao: Obesidade Grave || Grau de obesidade: 3`
+  }
+    
 })
 
 const year = document.getElementById('current-year')
