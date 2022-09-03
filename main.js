d47db17ba15dbd342bb2a628e29e6f6662e10c3e
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-09-03 17:19:06 -0300

    adicionando cores para exemplificar imc ideal

--- main.js
+++ main.js
@@ -21,22 +21,29 @@ verificar.addEventListener('click', function(event) {
   
   imc = imc.toFixed(1) //fixando resultado do imc só até a primeira casa decimal
   
+  
   if(!weight && !height || !name){
     alert('Preencha os campos acima corretamente') //Checa se os campos estão todos preenchidos e assim liberando a resposta
   } else {
+    
     if(imc < 18.5) {
+    res.style.color = '#08870C'
     res.innerHTML = `Ola ${name}! Seu imc é de: ${imc} e está entre ${resImc[0]} ou abaixo || Classificaçao: Magreza || Grau de obesidade: 0`
     
   } else if(imc >= 18.5 && imc <= 24.9) {
+    res.style.color = '#3D6E07'
     res.innerHTML = ` Olá ${name}! Seu imc é de ${imc} e está entre: ${resImc[1]} || Classificaçao: Normal || Grau de obesidade: 0`
     
   } else if (imc >= 25.0 && imc <= 29.9) {
+    res.style.color = '#B7960A'
     res.innerHTML = `Olá ${name}! Seu imc é de ${imc} e está entre ${resImc[2]} || Classificaçao: Sobrepeso || Grau de obesidade: 1`
     
   } else if (imc >= 30.0 && imc <= 39.9) {
+    res.style.color = '#D98005'
     res.innerHTML = `Olá ${name}! Seu imc é  de ${imc} e está entre ${resImc[3]} || Classificaçao: Obesidade || Grau de obesidade: 2`
   } else  {
     imc > 40.0;
+    res.style.color = '#FF0000'
     res.innerHTML = `Olá ${name}! Seu imc é de ${imc} e está acima de 40,0 procure um médico urgente! || Classificaçao: Obesidade Grave || Grau de obesidade: 3`
   } //checagem para dizer se o imc esta ou não na medida certa e dizendo em qual classificação se adequa.
   }
